df32391da15ce3f1a4b513f749665370e35ccdd3
yumeki <yumeki1206@gmail.com>
2025-04-29 16:37:41 +0900

    crudのcreateとupdateを作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,12 +26,23 @@
     <main>
         <h1>投稿一覧</h1>
 
+        @if(session('flash_message'))
+            <p>{{ session('flash_message') }}</p>
+        @endif
+
+        @if(session('error_message'))
+            <p>{{ session('error_message')}}</p>
+        @endif
+
+        <a href="{{ route('posts.create') }}">新規投稿</a>
+
         @if($posts->isNotEmpty())
             @foreach($posts as $post)
                 <article>
                     <h2>{{ $post->title }}</h2>
                     <p>{{ $post->content }}</p>
                     <a href="{{ route('posts.show', $post) }}">詳細</a>
+                    <a href="{{ route('posts.edit', $post) }}">編集</a>
                 </article>
             @endforeach
         @else
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -25,11 +25,20 @@
 
     <main>
         <h1>投稿詳細</h1>
+
+        @if(session('flash_message'))
+            <p>{{ session('flash_message') }}</p>
+        @endif
+
         <a href="{{ route('posts.index') }}">&lt; 戻る</a>
 
         <article>
             <h2>{{ $post->title }}</h2>
             <p>{{ $post->content }}</p>
+
+            @if($post->user_id === Auth::id())
+                <a href="{{ route('posts.edit',$post) }}">編集</a>
+            @endif
         </article>
     </main>
 
