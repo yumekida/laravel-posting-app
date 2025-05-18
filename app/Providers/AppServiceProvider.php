85a0813fdf4cac2a7ef6a3964d88b534070b9c8c
yumeki <yumeki1206@gmail.com>
2025-05-18 12:58:23 +0900

    deploy to heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(App::environment(['production'])){
+            URL::forceScheme('https');
+        }
+
     }
 }
